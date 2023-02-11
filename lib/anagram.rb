7972dea0264a5c07cd84c00b1e817459db3dbb09
njokichristine <christinenjoki6900@gmail.com>
2023-02-11 18:41:16 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(array)
+      array.select do |arr|
+        arr.split("").sort == @word.split("").sort
+      end
+    end
+  
+  
+  end
